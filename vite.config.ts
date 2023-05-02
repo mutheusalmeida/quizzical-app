acccce6bdecb299cde78d2def7b00f613a2f11bc
Matheus <matheusalmeida016@gmail.com>
2023-05-02 11:09:20 -0400

    feat: add base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig(configEnv => ({
+  base: '/',
   plugins: [
     react(),
     tsConfigPaths(),
